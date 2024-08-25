2946100121c791dd1ace52af725ab9c019d1f65a
oncelerr <markjonathan368@gmail.com>
2024-08-25 15:22:07 +0800

    fix slider

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -55,7 +55,7 @@ export default function Home() {
     gsap.set(firstText.current, {xPercent: xPercent})
     gsap.set(secondText.current, {xPercent: xPercent})
     requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
+    xPercent += 0.03 * direction; // Reduced increment for slower movement
   }
 
   return (
