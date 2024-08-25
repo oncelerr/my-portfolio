26275d3296f134f81cf83ca8bbfff7a8aca52395
oncelerr <markjonathan368@gmail.com>
2024-08-25 10:58:08 +0800

    Update index.jsx

--- src/components/Description/index.jsx
+++ src/components/Description/index.jsx
@@ -2,29 +2,41 @@ import styles from './style.module.scss';
 import { useInView, motion } from 'framer-motion';
 import { useRef } from 'react';
 import { slideUp, opacity } from './animation';
-import Rounded from '../../common/RoundedButton';
-export default function index() {
+import RoundedButton from '../../common/RoundedButton';
 
+export default function Description() {
     const phrase = "Empowering your digital journey with innovative solutions. Let's redefine what's possible, staying sharp, efficient, and ahead of the curve.";
-    const description = useRef(null);
-    const isInView = useInView(description)
+    const descriptionRef = useRef(null);
+    const isInView = useInView(descriptionRef, { once: true });
+
     return (
-        <div ref={description} className={styles.description}>
+        <div ref={descriptionRef} className={styles.description}>
             <div className={styles.body}>
                 <p>
-                {
-                    phrase.split(" ").map( (word, index) => {
-                        return <span key={index} className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"} key={index}>{word}</motion.span></span>
-                    })
-                }
+                    {phrase.split(" ").map((word, index) => (
+                        <span key={index} className={styles.mask}>
+                            <motion.span
+                                variants={slideUp}
+                                custom={index}
+                                animate={isInView ? "open" : "closed"}
+                            >
+                                {word}
+                            </motion.span>
+                        </span>
+                    ))}
                 </p>
-                <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>The fusion of my expertise in cybersecurity, development, and design places me at the forefront of creating secure, innovative digital experiences.</motion.p>
+                <motion.p
+                    variants={opacity}
+                    animate={isInView ? "open" : "closed"}
+                >
+                    The fusion of my expertise in cybersecurity, development, and design places me at the forefront of creating secure, innovative digital experiences.
+                </motion.p>
                 <div data-scroll data-scroll-speed={0.1}>
-                    <Rounded className={styles.button}>
+                    <RoundedButton className={styles.button}>
                         <p>About me</p>
-                    </Rounded>
+                    </RoundedButton>
                 </div>
             </div>
         </div>
-    )
+    );
 }
